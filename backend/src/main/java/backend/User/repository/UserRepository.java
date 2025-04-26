19da565be7348606dfb0f0f1059b5622d97f7eff
Chanuka Devin <110799877+Malshika1216@users.noreply.github.com>
2025-04-26 22:44:36 +0530

    Update UserRepository.java

--- backend/src/main/java/backend/User/repository/UserRepository.java
+++ backend/src/main/java/backend/User/repository/UserRepository.java
@@ -9,5 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends MongoRepository<UserModel, String> {
     Optional<UserModel> findByEmail(String email);
+
     boolean existsByEmail(String email);
 }
